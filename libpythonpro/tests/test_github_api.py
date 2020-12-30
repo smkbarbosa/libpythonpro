5d3f9cf86462d245927f293872a2e80c0edd70ba
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-12-30 02:00:04 -0300

    corrige testes

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -6,22 +6,23 @@ from libpythonpro import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = "https://avatars0.githubusercontent.com/u/6677139?v=4"
     resp_mock.json.return_value = {
-        "login":               "smkbarbosa",
-        "id":                  6677139,
-        "avatar_url":          url
+        "login": "smkbarbosa",
+        "id": 6677139,
+        "avatar_url": url
         }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('smkbarbosa')
     assert avatar_url == url
+
 
 def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('manus-consultoria')
