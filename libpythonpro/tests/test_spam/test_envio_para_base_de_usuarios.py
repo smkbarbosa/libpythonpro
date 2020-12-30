58b4de65f8e851d529a3f9a03e509af78b7b4d3a
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-12-30 00:35:09 -0300

    injeção de dependencias + intro mock

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -5,6 +5,17 @@ from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.models import Usuario
 
 
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
 @pytest.mark.parametrize(
     'usuarios',
     [
@@ -20,7 +31,7 @@ from libpythonpro.spam.models import Usuario
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'contato@smkbarbosa.eti.br',
@@ -28,3 +39,21 @@ def test_qtde_de_spam(sessao, usuarios):
         'Confira os módulos fantásticos'
         )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Samuel', email='contato@smkbarbosa.eti.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'samuka1@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+        )
+    assert enviador.parametros_de_envio == (
+        'samuka1@gmail.com',
+        'contato@smkbarbosa.eti.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
